4f396b96d251db081c7269178fd1a065d29a6fb7
ChrisP-Git <chrisp@ici-bas.org>
2017-05-12 23:56:56 +0200

    Update and improve
    Add I2C LCD 1602 support for real time temperature, humidity, AQI and PM1.0 access
    Change PMS 5003 UART to Serial2, so that uploading the sketch does not need to unplug GPIO3 anymore.
    and various bug fixes

--- PartSensor.ino
+++ PartSensor.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <DHT.h>
 #include <ESP8266WiFi.h>
+#include <LiquidCrystal_I2C.h>
 
 #define DEBUG
 #ifdef DEBUG
@@ -10,6 +11,9 @@
 #define DEBUG_PRINTLN(x)
 #define DEBUG_PRINT(x)
 #endif
+
+// I2C LCd
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // ThingSpeak
 // PM Channel:
@@ -43,54 +47,71 @@ const char* cWifiPassword = "PASSWORD";
 const int MAX_WIFI_TRY = 4;
 
 //DHT22
-#define PINDHT 4 // what pin DHT sensor is connected to
+//#define PINDHT 4 // what pin we’re connected to
+const int PINDHT = D5;
 float humidity = 0;
-float temperature = 0;
+float temperature=0;
 
 //PMS5003
+// #define PINPMS 5 // what pin we’re connected to for RX
+const int PINPMSGO = D0; // what pin we’re connected to activate measurement
+
+//#define CONTINUOUS_MODE //read data continously and print to serial, no sensor deep sleep in continuous mode requred
+#define NETWORK_MODE //no network required in debug mode, data will be printed to serial 
+
 #define MSG_LENGTH 31   //0x42 + 31 bytes equal to PMS5003 serial message packet lenght
 #define HTTP_TIMEOUT 20000 //maximum http response wait period, sensor disconects if no response
 #define MIN_WARM_TIME 35000 //warming-up period requred for sensor to enable fan and prepare air chamber
 unsigned char buf[MSG_LENGTH];
 
-int atmPM01Value = 0;  //define Atmospheric PM1.0 value of the air detector module
-int atmPM25Value = 0;  //define Atmospheric PM2.5 value of the air detector module
-int atmPM10Value = 0;  //define Atmospheric PM10 value of the air detector module
-int CF1PM01Value = 0;  //define CF1 PM1.0 value of the air detector module
-int CF1PM25Value = 0;  //define CF1 PM2.5 value of the air detector module
-int CF1PM10Value = 0;  //define CF1 PM10 value of the air detector module
-int Partcount0_3 = 0;  //define Particule Count > 0.3 µm
-int Partcount0_5 = 0;  //define Particule Count > 0.5 µm
-int Partcount1_0 = 0;  //define Particule Count > 1.0 µm
-int Partcount2_5 = 0;  //define Particule Count > 2.5 µm
-int Partcount5_0 = 0;  //define Particule Count > 5.0 µm
-int Partcount10 = 0;   //define Particule Count > 10.0 µm
-int airQualityIndex = 0;// Calulated Air Quality Index
+int atmPM01Value = 0;  //define PM1.0 value of the air detector module
+int atmPM25Value = 0;  //define pm2.5 value of the air detector module
+int atmPM10Value = 0;  //define pm10 value of the air detector module
+int CF1PM01Value = 0;  //define PM1.0 value of the air detector module
+int CF1PM25Value = 0;  //define pm2.5 value of the air detector module
+int CF1PM10Value = 0;  //define pm10 value of the air detector module
+int Partcount0_3 = 0;
+int Partcount0_5 = 0;
+int Partcount1_0 = 0;
+int Partcount2_5 = 0;
+int Partcount5_0 = 0;
+int Partcount10 = 0;
+
+int airQualityIndex = 0;
+
+
+
+
+DHT dht(PINDHT, DHT22);
+WiFiClient client;
 
 void setupWIFI() {
+#ifdef NETWORK_MODE
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
   WiFi.begin(cWifiSSID, cWifiPassword);
-  int WifiTry = 0;
+  int i = 0;
   DEBUG_PRINTLN("MAC address: ");
   DEBUG_PRINTLN(WiFi.macAddress());
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(2000);
-    WifiTry++;
-	// Try to connect until success. If too many failed try, force ESP Wifi reset
-    if (WifiTry > MAX_WIFI_TRY) {
-          WifiTry = 0;
+    i++;
+    if (i > MAX_WIFI_TRY) {
+          i = 0;
           DEBUG_PRINTLN("WiFi failed, retry");
           Serial.printf("WiFi status:%d\n",  WiFi.status());
+          //ESP.reset();
+          
           WiFi.disconnect();
           WiFi.persistent(false);
           WiFi.mode(WIFI_OFF);
           WiFi.mode(WIFI_STA);
           WiFi.begin(cWifiSSID, cWifiPassword);
       }
+    
     DEBUG_PRINT(".");
   }
 
@@ -98,10 +119,10 @@ void setupWIFI() {
   DEBUG_PRINTLN("WiFi connected");
   DEBUG_PRINTLN("IP address: ");
   DEBUG_PRINTLN(WiFi.localIP());
+#endif
 }
 
-boolean validatePMS5003frame() {
-
+boolean validateMsg() {
   int receiveSum = 0;
   for (int i = 0; i < (MSG_LENGTH - 2); i++) {
     receiveSum = receiveSum + buf[i];
@@ -109,6 +130,56 @@ boolean validatePMS5003frame() {
   receiveSum = receiveSum + 0x42;
   return receiveSum == ((buf[MSG_LENGTH - 2] << 8) + buf[MSG_LENGTH - 1]);
 }
+
+//Decode frame values
+int decodeAtmosphericPM01(unsigned char *thebuf) {
+  return ((thebuf[3] << 8) + thebuf[4]); //count PM1.0 atmospheric value of the air detector module
+}
+
+int decodeCF1PM01(unsigned char *thebuf) {
+  return ((thebuf[9] << 8) + thebuf[10]); //count PM1.0 atmospheric value of the air detector module
+}
+
+int decodeAtmosphericPM25(unsigned char *thebuf) {
+  return ((thebuf[11] << 8) + thebuf[12]); //count pm2.5 atmospheric value of the air detector module
+}
+
+int decodeCF1PM25(unsigned char *thebuf) {
+  return ((thebuf[5] << 8) + thebuf[6]); //count pm2.5 atmospheric value of the air detector module
+}
+  
+int decodeAtmosphericPM10(unsigned char *thebuf) {
+  return ((thebuf[13] << 8) + thebuf[14]); //count pm10 atmospheric value of the air detector module
+}
+
+int decodeCF1PM10(unsigned char *thebuf) {
+  return ((thebuf[7] << 8) + thebuf[8]); //count pm10 atmospheric value of the air detector module
+}
+
+int decodeCount0_3(unsigned char *thebuf) {
+  return ((thebuf[15] << 8) + thebuf[16]); //count pm10 atmospheric value of the air detector module
+}
+
+int decodeCount0_5(unsigned char *thebuf) {
+  return ((thebuf[17] << 8) + thebuf[18]); //count pm10 atmospheric value of the air detector module
+}
+
+int decodeCount1_0(unsigned char *thebuf) {
+  return ((thebuf[19] << 8) + thebuf[20]); //count pm10 atmospheric value of the air detector module
+}
+
+int decodeCount2_5(unsigned char *thebuf) {
+  return ((thebuf[21] << 8) + thebuf[22]); //count pm10 atmospheric value of the air detector module
+}
+
+int decodeCount5_0(unsigned char *thebuf) {
+  return ((thebuf[23] << 8) + thebuf[24]); //count pm10 atmospheric value of the air detector module
+}
+
+int decodeCount10(unsigned char *thebuf) {
+  return ((thebuf[25] << 8) + thebuf[26]); //count pm10 atmospheric value of the air detector module
+}
+
 
 // AQI formula: https://en.wikipedia.org/wiki/Air_Quality_Index#United_States
 int toAQI(int I_high, int I_low, int C_high, int C_low, int C) {
@@ -142,17 +213,54 @@ int calculateAQI25(float density) {
   }
 }
 
-void powerOnPMS5003() {
-  digitalWrite(D0, HIGH);
+void powerOnSensor() {
+  digitalWrite(PINPMSGO, HIGH);
 }
 
-void powerOffPMS5003() {
+void powerOffSensor() {
+#ifdef NETWORK_MODE
   WiFi.disconnect();
-
-  digitalWrite(D0, LOW);
+#endif
+#ifndef CONTINUOUS_MODE
+  digitalWrite(PINPMSGO, LOW);
   //ESP.deepSleep(SLEEP_TIME * 1000000); //deep sleep in microseconds, unfortunately doesn't work properly
   DEBUG_PRINTLN("going to sleep zzz...");
   delay(SLEEP_TIME);
+#endif
+}
+
+void ShowDataonLCD() {
+    char buffer[35]; 
+    if (!isnan(temperature)) {
+      lcd.setCursor(0,0);
+      float f=temperature;
+      sprintf(buffer, "T:%d.%01dC ",(int)f, (int)(f*10)%10);
+      lcd.print(buffer);
+    }
+    else {
+      lcd.setCursor(0,0);
+      lcd.print("T: NaN");
+    }
+    
+    if (!isnan(humidity)) {
+      lcd.setCursor(8,0);
+      float f=humidity;
+      sprintf(buffer, "H:%d.%01d%%",(int)f, (int)(f*10)%10);
+      lcd.print(buffer);
+    }  
+    else {
+      lcd.setCursor(7,0);
+      lcd.print("H: NaN");
+    }
+
+    lcd.setCursor(0,1);
+    sprintf(buffer, "AQ:%d ",(int)airQualityIndex);
+    lcd.print(buffer);
+
+    lcd.setCursor(7,1);
+    sprintf(buffer, "PM:%d ",(int)CF1PM01Value);
+    lcd.print(buffer);
+
 }
 
 void sendPMDataToCloud() {
@@ -210,6 +318,8 @@ void sendRawDataToCloud() {
 }
 
 void sendDataToCloud(String apikey, String strfield) {
+#ifdef NETWORK_MODE // wifi connection is required only in network mode
+
   DEBUG_PRINTLN("sendDataToCloud start");
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
@@ -243,7 +353,7 @@ void sendDataToCloud(String apikey, String strfield) {
     }
   }
 
-  // Read all the lines of the reply from server and print them to Serial Debug
+  // Read all the lines of the reply from server and print them to Serial
   while (client.available()) {
     String line = client.readStringUntil('\r');
     DEBUG_PRINT(line);
@@ -251,24 +361,39 @@ void sendDataToCloud(String apikey, String strfield) {
 
   client.stop();
   DEBUG_PRINTLN("closing connection");
+#endif
 }
+
+
 
 void setup() {
   Serial.begin(9600);
+
+
+  
+  
+  // The begin call takes the width and height. This
+  // Should match the number provided to the constructor.
+  DEBUG_PRINTLN("Initialize LCD");
+  lcd.begin(16,2);
+  lcd.init();
+
+  // Turn on the backlight.
+  lcd.backlight();
+  lcd.print("Initialize ...");
+  DEBUG_PRINTLN("LCD Initialized");
+
+
+
+  
   
   delay(10);
-  DHT dht(PINDHT, DHT22);
-  WiFiClient client;
-  
   dht.begin();
 
-#ifdef DEBUG
-  Serial.println(" Init started: DEBUG MODE");
-#else
-  Serial.println(" Init started: NO DEBUG MODE");
-#endif
-  Serial.setTimeout(1500); //set the Timeout to 1500ms, longer than the data transmission time of the sensor
-  pinMode(D0, OUTPUT);
+  DEBUG_PRINTLN(" Init started: DEBUG MODE");
+
+  Serial.setTimeout(1500);//set the Timeout to 1500ms, longer than the data transmission time of the sensor
+  pinMode(PINPMSGO, OUTPUT);
   DEBUG_PRINTLN("Initialization finished");
 }
 
@@ -276,45 +401,56 @@ void loop() {
   DEBUG_PRINTLN("loop start");
   unsigned long timeout = millis();
 
-  powerOnPMS5003();
+  powerOnSensor();
   setupWIFI();
 
+#ifndef CONTINUOUS_MODE //no warm-up required for continous mode
   timeout = MIN_WARM_TIME - (millis() - timeout);
   if (timeout > 0) {
     DEBUG_PRINT("sensor warm-up: ");
     DEBUG_PRINTLN(timeout);
-    delay(timeout);
+//    delay(timeout);
   }
+#endif
 
   humidity = dht.readHumidity();
   temperature = dht.readTemperature();
   if (isnan(humidity) || isnan(temperature)) {
-    Serial.print("DHT22 not ready, skipped");
+    DEBUG_PRINTLN("DHT22 not ready, skipped");
   }
 
 
   for (int i = 0; i < 30; i++) {
+  Serial.swap(); //Use UART2 for PMS5003 communication (Allow sketch upload without having to unplug GPIO3 !!!!)
     if (Serial.find(0x42)) {  //start to read when detect 0x42
       Serial.readBytes(buf, MSG_LENGTH);
+    Serial.swap();
 
-      if (buf[0] == 0x4d && validatePMS5003frame()) {
-		// PMS5003 frame format detail from http://www.aqmd.gov/docs/default-source/aq-spec/resources-page/plantower-pms5003-manual_v2-3.pdf?sfvrsn=2
-        CF1PM01Value = (thebuf[9] << 8) + thebuf[10]; //PM1.0 CF1 value of the air detector module
-        CF1PM25Value = (thebuf[5] << 8) + thebuf[6]; //PM2.5 CF1 value of the air detector module
-        CF1PM10Value = (thebuf[7] << 8) + thebuf[8]; //PM10 CF1 value of the air detector module
+      if (buf[0] == 0x4d && validateMsg()) {
+        CF1PM01Value = decodeCF1PM01(buf); //count PM1.0 CF1 value of the air detector module
+        CF1PM25Value = decodeCF1PM25(buf);//count pm2.5 CF1 value of the air detector module
+        CF1PM10Value = decodeCF1PM10(buf); //count pm10 CF1 value of the air detector module
 
-        atmPM01Value = (thebuf[3] << 8) + thebuf[4]; //PM1.0 atmospheric value of the air detector module
-        atmPM25Value = (thebuf[11] << 8) + thebuf[12]; //PM2.5 atmospheric value of the air detector module
-        atmPM10Value = (thebuf[13] << 8) + thebuf[14]; //PM10 atmospheric value of the air detector module
+        atmPM01Value = decodeAtmosphericPM01(buf); //count PM1.0 atmospheric value of the air detector module
+        atmPM25Value = decodeAtmosphericPM25(buf);//count pm2.5 atmospheric value of the air detector module
+        atmPM10Value = decodeAtmosphericPM10(buf); //count pm10 atmospheric value of the air detector module
     
-        Partcount0_3 = (thebuf[15] << 8) + thebuf[16]; //count particules > 0.3 µm
-        Partcount0_5 = (thebuf[17] << 8) + thebuf[18]; //count particules > 0.5 µm
-        Partcount1_0 = (thebuf[19] << 8) + thebuf[20]; //count particules > 1.0 µm
-        Partcount2_5 = (thebuf[21] << 8) + thebuf[22]; //count particules > 2.5 µm
-        Partcount5_0 = (thebuf[23] << 8) + thebuf[24]; //count particules > 5.0 µm
-        Partcount10 = (thebuf[25] << 8) + thebuf[26]; //count particules > 10.0 µm
+        Partcount0_3 = decodeCount0_3(buf); //count 0.3 µm particulates count
+        Partcount0_5 = decodeCount0_5(buf); //count 0.5 µm particulates count
+        Partcount1_0 = decodeCount1_0(buf); //count 1.0 µm particulates count
+        Partcount2_5 = decodeCount2_5(buf); //count 2.5 µm particulates count
+        Partcount5_0 = decodeCount5_0(buf); //count 5.0 µm particulates count
+        Partcount10 = decodeCount10(buf); //count 10 µm particulates count
 
+        /*if (atmPM01Value == atmPM25Value || atmPM25Value == atmPM10Value) {
+          //it is very rarely happened that different particles have same value, better to read again
+          DEBUG_PRINT("skip loop:");
+          DEBUG_PRINTLN(i);
+          continue;
+        }
+        */
         airQualityIndex = calculateAQI25(atmPM25Value);
+
 
 #ifdef DEBUG //debug info only for serial connection debugging
         DEBUG_PRINTLN("Temperature: " + String(temperature));
@@ -337,16 +473,26 @@ void loop() {
 
         sendPMDataToCloud();
         sendRawDataToCloud();
+        ShowDataonLCD();
+#ifdef CONTINUOUS_MODE
+          delay(16000); // Wait more than 15 seconds to send additionnal data
+#endif
+
+
+#ifdef CONTINUOUS_MODE //loop forever for debugging purposes only
+        i = 0;
+#else
         break; //data processed, exit from for loop and sleep
+#endif //for CONTINUOUS_MODE
 
       } else {
         DEBUG_PRINTLN("message validation error");
       }
     } else {
-      DEBUG_PRINTLN("sensor msg start not found");
+    Serial.swap();
+    DEBUG_PRINTLN("sensor msg start not found");
     }
   }
 
-  powerOffPMS5003();
+  powerOffSensor();
 }
-
